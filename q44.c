0623d9add7380185e06b9596276e2793bf20c352
Aliva88 <250320100079@cutm.ac.in>
2025-08-30 21:42:58 +0530

    add

--- q44.c
+++ q44.c
@@ -8,23 +8,15 @@ int main()
     {
         case 'a':
         case 'A':
-        printf("A is vowel");
-        break;
         case 'e':
         case 'E':
-        printf("E is vowel");
-        break;
         case 'i':
         case 'I':
-        printf("I is vowel");
-        break;
         case 'o':
         case 'O':
-        printf("O is vowel");
-        break;
         case 'u':
         case 'U':
-        printf("U is vowel");
+        printf("The alphabate is vowel");
         break;
         default:
         printf("The alphabate is consonant");
