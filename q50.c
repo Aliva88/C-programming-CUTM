1aa7fd60e3237200118de838ad0810f13e7ae4a6
Aliva88 <250320100079@cutm.ac.in>
2025-08-30 22:23:06 +0530

    add

---
+++ q50.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,result;
+    printf("Enter any 2 floatng numbers ");
+    scanf("%f %f",&num1,&num2);
+    result = num1*num2;
+    printf("The product of 2 floating numbers is %f",result);
+    return 0;
+}
